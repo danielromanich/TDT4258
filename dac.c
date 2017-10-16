d98b0d132ea1515921a3e7d0cf7a9f3f5c9de0d3
danielromanich <daniel@romanich.com>
2017-10-16 12:53:27 +0200

    Cleanup

--- dac.c
+++ dac.c
@@ -11,16 +11,6 @@ void setupDAC()
 	*DAC0_CH1CTRL = 0x1;
 	*DAC0_CH0DATA = 0;
 	*DAC0_CH1DATA = 0;
-	/*
-	 * TODO enable and set up the Digital-Analog Converter
-	 * 
-	 * 1. Enable the DAC clock by setting bit 17 in CMU_HFPERCLKEN0 2.
-	 * Prescale DAC clock by writing 0x50010 to DAC0_CTRL 3. Enable left
-	 * and right audio channels by writing 1 to DAC0_CH0CTRL and
-	 * DAC0_CH1CTRL 4. Write a continuous stream of samples to the DAC
-	 * data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a
-	 * timer interrupt 
-	 */
 }
 
 void disableDAC()
--- ex2.c
+++ ex2.c
@@ -31,9 +31,6 @@ void setupNVIC();
 void setupGPIO();
 void playSongBW();
 
-/*
- * Your code will start executing here 
- */
 int main(void)
 {
 	/*
@@ -41,17 +38,10 @@ int main(void)
 	 */
 	setupGPIO();
 	setupDAC();
-	//setupTimer(SAMPLE_PERIOD); Not setting up timer because we want to deep sleep until we generate sounds
-
-	/*
-	 * Enable interrupt handling 
-	 */
-	clearInterrupts();
-	//setupNVIC();
 
 	*SCR = 0b0101;	//Enable deep sleep
 
-	//-----------------------------------------------------------------------------------------------
+	//Since we are polling we need to keep checking the button to see if something was pressed.
 	while(1){
 	    bool pressed = 1;
 		uint32_t value = (*GPIO_PC_DIN ^ 0xFFFFFFFF) & 0xFF;
@@ -63,23 +53,12 @@ int main(void)
 			chooseSong(&GOT, 0x3FFF);
 			break;
 		case BUTTON2:
-			chooseSong(&THATSNOMOON, 0x3FFF);
-			break;
 		case BUTTON3:
-			chooseSong(&THATSNOMOON, 0x3FFF);
-			break;
 		case BUTTON4:
-			chooseSong(&THATSNOMOON, 0x3FFF);
-			break;
 		case BUTTON5:
-			chooseSong(&THATSNOMOON, 0x3FFF);
-			break;
 		case BUTTON6:
-			chooseSong(&THATSNOMOON, 0x3FFF);
-			break;
 		case BUTTON7:
-			chooseSong(&THATSNOMOON, 0x3FFF);
-			break;
+		    break;
 		case BUTTON8:
 			stopSong();
 			break;
@@ -93,17 +72,5 @@ int main(void)
 		}
 	}
 	//-----------------------------------------------------------------------------------------------
-
-
-	__asm("wfi");		//Go into deep sleep
 	return 0;
-}
-
-void setupNVIC()
-{
-	*GPIO_EXTIPSELL = 0x22222222;
-	*GPIO_EXTIFALL = 0xFF;
-	*GPIO_EXTIRISE = 0xFF;
-	*GPIO_IEN = 0xFF;
-	*ISER0 |= 0x1802;
 }
